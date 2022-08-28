489413ce41c666aa0ac1823137743d6b01c0a253
gombouka <Tomondup266>
2022-08-28 12:23:26 +0200

    Working bot : test

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ try {
 			/** Get the last message > DB date */
 			for (const message of json) {
 				if (message.author.id === '952125649345196044' && message.interaction.user.id === account.id && message.interaction.name === 'work') {
-					money_mess_date = getLocaleDate( message.timestamp );
-					console.log( `Money message ${money_mess_date} | Last in DB ${account_data.date}, Timestamp : ${message.timestamp} | Gain : ${parseInt( message.content.split( '**' )[1] )} | Date : ${getLocaleDateString()}` );
+					money_mess_date = new Date( message.timestamp );
+					console.log( `Money message ${money_mess_date} ${money_mess_date.toUTCString()} | Last in DB ${new Date(account_data.date)}, Timestamp : ${message.timestamp} | Gain : ${parseInt( message.content.split( '**' )[1] )} | Date : ${getLocaleDateString()}` );
 					if (money_mess_date > data.date) return parseInt( message.content.split( '**' )[1] );
 				}
 			}
