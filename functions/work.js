582cb2e33a8a1848afcdd5087929374ff323c7d1
gombouka <Tomondup266>
2022-09-10 00:39:38 +0200

    Working bot : Works only the day

--- functions/work.js
+++ functions/work.js
@@ -63,7 +63,7 @@ export default class WorkHandler {
 		getActivity( this.id, this.authorization ).then( activity => {
 			
 			if (this.pay) {
-				if (activity < 6 || !this.day.includes( getLocaleDate().getHours() ) && this.getChance( 40 )) {
+				if (activity < 6 && this.day.includes( getLocaleDate().getHours() ) || !this.day.includes( getLocaleDate().getHours() ) && this.getChance( 40 )) {
 					return this.workRetry( 'activity '.red() + activity );
 				}
 			} else {
